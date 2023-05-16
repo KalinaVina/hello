0ff4e701a755a7aa61a44f668f154d3b0dc6cbe2
KalinaVina <dariya.kalvina@gmail.com>
2023-05-16 14:26:24 +0300

    @

---
+++ src/Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+
+        int res1 = num1+num2;
+        int res2 = num1-num2;
+        int res3 = num1/num2;
+        int res4 = num1*num2;
+
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
+        System.out.println(res1+res2);
+    }
+
+}
